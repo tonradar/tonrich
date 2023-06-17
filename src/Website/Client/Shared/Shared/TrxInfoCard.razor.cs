136078d456f1e970b6b4419f20ccef8e18bd870d
Maryam Davoudi <davoudi.maryam@outlook.com>
2023-06-17 17:06:34 +0330

    Set loading sturcture and trxInfoCard component #25092

---
+++ src/Website/Client/Shared/Shared/TrxInfoCard.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonrich.Client.Shared.Shared
+{
+    public partial class TrxInfoCard
+    {
+        [Parameter] public string? IconName { get; set; }
+        [Parameter] public string? Title { get; set; }
+        [Parameter] public decimal? Value { get; set; }
+        [Parameter] public string? Unit { get; set; }
+        [Parameter] public string? Description { get; set; }
+        [Parameter] public bool IsBusy { get; set; } = true;
+    }
+}
