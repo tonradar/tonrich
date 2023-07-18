1c466b6a4a16b0ddc8beab129299e85e6bd4d543
Hamed Hajiloo <hamedhajiloo@hotmail.com>
2023-07-18 13:17:44 +0330

    Get Nft prices via Api #25103

--- src/Website/Client/Web/Program.cs
+++ src/Website/Client/Web/Program.cs
@@ -30,7 +30,12 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault();
         builder.Configuration.AddJsonStream(typeof(MainLayout).Assembly.GetManifestResourceStream("Tonrich.Client.Shared.appsettings.json"));
 
-        builder.Services.AddSingleton(sp => new HttpClient(sp.GetRequiredService<AppHttpClientHandler>()) { BaseAddress = new Uri(builder.Configuration.GetApiServerAddress()) });
+        var apiServerAddressConfig = builder.Configuration.GetApiServerAddress();
+
+        var apiServerAddress = new Uri($"{builder.HostEnvironment.BaseAddress}{apiServerAddressConfig}");
+
+        builder.Services.AddSingleton(sp => new HttpClient(sp.GetRequiredService<AppHttpClientHandler>()) { BaseAddress = apiServerAddress });
+
         builder.Services.AddScoped<Microsoft.AspNetCore.Components.WebAssembly.Services.LazyAssemblyLoader>();
         builder.Services.AddTransient<IAuthTokenProvider, ClientSideAuthTokenProvider>();
 
---
+++ src/Website/Server/Api/Controllers/NFTController.cs
@@ -0,0 +1,20 @@
+﻿namespace Tonrich.Server.Api.Controllers;
+
+[AllowAnonymous]
+[Route("api/[controller]/[action]")]
+[ApiController]
+public partial class NFTController : AppControllerBase
+{
+    [AutoInject] protected ITonService TonService;
+    [HttpPost]
+    public async Task<decimal> GetUserNamesPriceAsync(IEnumerable<string> userNames, CancellationToken cancellationToken)
+    {
+        return await TonService.GetUserNamesPriceAsync(userNames, cancellationToken);
+    }
+
+    [HttpPost]
+    public async Task<decimal> GetNumbersPriceAsync(IEnumerable<string> numbers, CancellationToken cancellationToken)
+    {
+        return await TonService.GetNumbersPriceAsync(numbers, cancellationToken);
+    }
+}
