e730389f8999c05fcfe9b2ab8aeb841fb2e5f3ee
Hamed Hajiloo <hamedhajiloo@hotmail.com>
2023-07-22 22:57:22 +0330

    Show swagger in production mode #25104

--- src/Website/Tonrich.Job.ScreenShooter/Program.cs
+++ src/Website/Tonrich.Job.ScreenShooter/Program.cs
@@ -17,11 +17,8 @@ builder.Services.AddScoped<ISeleniumService, SeleniumService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
