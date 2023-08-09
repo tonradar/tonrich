8af56e3955be28c6fd24aeb25d205d095aa41a09
Hamed Hajiloo <hamedhajiloo@hotmail.com>
2023-08-09 13:07:56 +0330

    Close plugin if clicked outside of plugin

--- src/Plugin/client/contentScripts/tonrich.ts
+++ src/Plugin/client/contentScripts/tonrich.ts
@@ -1,27 +1,34 @@
 abstract class tonrich {
-   abstract addBadge(): void;
-   
-   showSite(url: string, element: HTMLElement, style: string) {
+  abstract addBadge(): void;
+
+  showSite(url: string, element: HTMLElement, style: string) {
+    var iframe = document.getElementById('tonapi-iframe');
+    if (iframe) {
+      iframe.remove();
+    }
+
+    var newIframe = document.createElement('iframe') as HTMLIFrameElement;
+
+    newIframe.classList.add(style);
+
+    newIframe.id = 'tonapi-iframe';
+    newIframe.src = url;
+    newIframe.addEventListener('mouseleave', (e) => {
       var iframe = document.getElementById('tonapi-iframe');
       if (iframe) {
         iframe.remove();
       }
-    
-      var newIframe = document.createElement('iframe') as HTMLIFrameElement;
-    
-      newIframe.classList.add(style);
-    
-      newIframe.id = 'tonapi-iframe';
-      newIframe.src = url;
-      newIframe.addEventListener('mouseleave', (e) => {
-        var iframe = document.getElementById('tonapi-iframe');
-        if (iframe) {
-          iframe.remove();
-        }
-      });
-    
-      element.appendChild(newIframe);
-    }
+    });
+
+    element.appendChild(newIframe);
+
+    document.addEventListener('click', function (event) {
+      var iframe = document.getElementById('tonapi-iframe');
+      if (iframe && !iframe.contains(event.target as Node)) {
+        iframe.remove();
+      }
+    });
+  }
 }
 
 export default tonrich;
