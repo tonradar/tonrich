8c241eebc0148e8eb45b022f8341132eba31a4d1
Maryam Davoudi <davoudi.maryam@outlook.com>
2023-08-29 16:42:40 +0330

    Add recap tonrich component #26941

---
+++ src/Website/Client/Shared/Components/RecapTonrich.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System.Text.Json;
+using Tonrich.Shared.Util;
+
+namespace Tonrich.Client.Shared.Components;
+
+public partial class RecapTonrich
+{
+}
